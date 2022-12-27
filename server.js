6c53f5c90f65b325e004130190981737cb93d962
Brock Fanning <brockfanning@gmail.com>
2022-12-27 01:14:16 -0500

    Progress

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 const fetch = (...args) =>
     import('node-fetch').then(({default: fetch}) => fetch(...args));
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-const CLIENT_ID = 'd057ffc62f01ce8f3376';
+const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 var app = express();
@@ -15,30 +15,14 @@ app.use(bodyParser.json());
 app.get('/getAccessToken', async function (req, res) {
 
     const params = '?client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&code=' + req.query.code;
-
-    await fetch('https://github.com/login/oauth/access_token' + params, {
+    const response = await fetch('https://github.com/login/oauth/access_token' + params, {
         method: 'POST',
         headers: {
             'Accept': 'application/json'
         }
-    }).then((response) => {
-        return response.json();
-    }).then((data) => {
-        res.json(data);
     });
-});
-
-app.get('/getUserData', async function (req, res) {
-    await fetch('https://api.github.com/user', {
-        method: 'GET',
-        headers: {
-            'Authorization': req.get('Authorization')
-        }
-    }).then((response) => {
-        return response.json();
-    }).then((data) => {
-        res.json(data);
-    });
+    const data = await response.json();
+    return res.json(data);
 });
 
 app.listen(process.env.PORT);
